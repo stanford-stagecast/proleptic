9e7f7286871259bb8cbd2fa0cd2ba6f59092c1c7
Keith Winstein <keithw@cs.stanford.edu>
2022-07-18 13:06:57 -0500

    Simple benchmark

--- src/ex1.cc
+++ src/ex1.cc
@@ -1,39 +1,46 @@
 #include <iostream>
+#include <string>
 
 #include "network.hh"
 #include "pp.hh"
 
 using namespace std;
 
-int main()
+void program_body( const string& iterations )
 {
-  using DNN = Network<float, 1, 2, 2, 1>;
+  const unsigned int num_iterations = stoi( iterations );
+
+  using DNN = Network<float, 1, 16, 128, 128, 128, 128, 128, 128, 128, 128, 1>;
   DNN mynetwork;
 
   DNN::A_storage s1, s2, s3;
 
   DNN::M_input& input = *reinterpret_cast<DNN::M_input*>( s1.data() );
 
-  input( 0, 0 ) = 8;
-  input( 0, 1 ) = -16;
+  input( 0, 0 ) = 15;
 
-  auto& layer1 = mynetwork.layer0;
-  auto& layer2 = mynetwork.next.layer0;
-
-  layer1.weights().setOnes();
-  layer1.biases().setOnes();
-  layer2.weights().setOnes();
-  layer2.biases().setOnes();
-
-  const DNN::M_output& output = mynetwork.apply( s1, s2, s3 );
-
-  pp( input );
-  pp( layer1.weights() );
-  pp( layer1.biases() );
-  pp( layer2.weights() );
-  pp( layer2.biases() );
-  pp( output );
+  for ( unsigned int i = 0; i < num_iterations; ++i ) {
+    mynetwork.apply( s1, s2, s3 );
+  }
 }
 
-// the i,j element of the resulting matrix is the sum of row i of the left matrix times column j of the right matrix
-// need same number of rows of the first matrix as columns of the second matrix
+int main( int argc, char* argv[] )
+{
+  if ( argc < 0 ) {
+    abort();
+  }
+
+  if ( argc != 2 ) {
+    cerr << "Usage: " << argv[0] << " iterations\n";
+    return EXIT_FAILURE;
+  }
+
+  try {
+    program_body( argv[1] );
+  } catch ( const exception& e ) {
+    cerr << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
