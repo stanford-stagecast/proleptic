9a6e9a1e74bb4d64e54d92ccc902a90b72de53e1
Keith Winstein <keithw@cs.stanford.edu>
2022-07-18 13:39:30 -0500

    cflangs.cmake: Disable Eigen stack limit

--- etc/cflags.cmake
+++ etc/cflags.cmake
@@ -2,4 +2,4 @@ set (CMAKE_CXX_STANDARD 20)
 set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
 set (CMAKE_BASE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 
-add_compile_options(-pedantic -pedantic-errors -Werror -Wall -Wextra -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Weffc++ -ffast-math -march=native -mtune=native)
+add_compile_options(-pedantic -pedantic-errors -Werror -Wall -Wextra -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Weffc++ -ffast-math -march=native -mtune=native -DEIGEN_STACK_ALLOCATION_LIMIT=0)
