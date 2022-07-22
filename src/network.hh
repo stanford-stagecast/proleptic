9cb0e7607a3ce92a974fd415ed94720f7f508abb
Keith Winstein <keithw@cs.stanford.edu>
2022-07-22 12:14:25 -0700

    network.hh: reorganize into class with public/private sections

--- src/network.hh
+++ src/network.hh
@@ -8,13 +8,11 @@ using namespace std;
 
 // recursive definition (base case below)
 template<typename T, size_t T_batch_size, size_t i0, size_t o0, size_t... o_rest>
-struct Network
+class Network
 {
+public:
   using L_layer0 = Layer<T, T_batch_size, i0, o0>;
   using N_rest = Network<T, T_batch_size, o0, o_rest...>;
-
-  L_layer0 layer0 {};
-  N_rest rest {};
 
   static constexpr size_t num_layers = N_rest::num_layers + 1;
   static constexpr size_t input_size = i0;
@@ -22,9 +20,6 @@ struct Network
 
   using M_input = typename L_layer0::M_input;
   using M_output = typename N_rest::M_output;
-  using M_output_this_layer = typename L_layer0::M_output;
-
-  M_output_this_layer unactivated_output_this_layer {}, activated_output_this_layer {};
 
   void apply( const M_input& input, M_output& output )
   {
@@ -32,15 +27,22 @@ struct Network
     layer0.activate( unactivated_output_this_layer, activated_output_this_layer );
     rest.apply( activated_output_this_layer, output );
   }
+
+private:
+  L_layer0 layer0 {};
+  N_rest rest {};
+
+  using M_output_this_layer = typename L_layer0::M_output;
+
+  M_output_this_layer unactivated_output_this_layer {}, activated_output_this_layer {};
 };
 
 // base case
 template<class T, size_t T_batch_size, size_t i0, size_t o0>
-struct Network<T, T_batch_size, i0, o0>
+class Network<T, T_batch_size, i0, o0>
 {
+public:
   using L_layer0 = Layer<T, T_batch_size, i0, o0>;
-
-  L_layer0 layer0 {};
 
   static constexpr size_t num_layers = 1;
   static constexpr size_t input_size = L_layer0::input_size;
@@ -48,7 +50,9 @@ struct Network<T, T_batch_size, i0, o0>
 
   using M_input = typename L_layer0::M_input;
   using M_output = typename L_layer0::M_output;
-  using M_output_this_layer = typename L_layer0::M_output;
 
   void apply( const M_input& input, M_output& output ) { layer0.apply_without_activation( input, output ); }
+
+private:
+  L_layer0 layer0 {};
 };
