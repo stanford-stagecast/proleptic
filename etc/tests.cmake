79c997f679cb1765a6dbcd308b974925cd74b756
Keith Winstein <keithw@cs.stanford.edu>
2022-07-26 14:00:20 -0700

    Add serdes test to cmake checks

--- etc/tests.cmake
+++ etc/tests.cmake
@@ -1,3 +1,5 @@
-#enable_testing ()
+enable_testing ()
 
-#add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --timeout 10 -R '^t_')
+add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --timeout 10 -R '^t_')
+
+add_test(NAME t_serdes_roundtrip COMMAND serdes-roundtrip)
