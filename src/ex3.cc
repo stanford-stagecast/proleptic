e11d7a290f506c3f2159156acf2ffcd7093c73f9
Keith Winstein <keithw@cs.stanford.edu>
2022-07-27 14:03:16 -0700

    Ex3 serves graph of NN performance via HTTP

--- src/ex3.cc
+++ src/ex3.cc
@@ -1,6 +1,14 @@
+#include "dnn_types.hh"
 #include "eventloop.hh"
 #include "exception.hh"
+#include "grapher.hh"
 #include "http_server.hh"
+#include "mmap.hh"
+#include "network.hh"
+#include "parser.hh"
+#include "random.hh"
+#include "sampler.hh"
+#include "serdes.hh"
 #include "socket.hh"
 
 #include <iostream>
@@ -8,12 +16,45 @@
 using namespace std;
 
 static constexpr size_t mebi = 1024 * 1024;
+static constexpr float note_timing_variation = 0.05; /* jitter = stddev of +/- 5% of the beat */
 
-int main()
+void program_body( const string& filename )
 {
-  FileDescriptor output { CheckSystemCall( "dup", dup( STDOUT_FILENO ) ) };
-  output.set_blocking( false );
+  ios::sync_with_stdio( false );
 
+  auto mynetwork_ptr = make_unique<DNN>();
+  DNN& mynetwork = *mynetwork_ptr;
+
+  // parse DNN from file
+  {
+    ReadOnlyFile dnn_on_disk { filename };
+    Parser parser { dnn_on_disk };
+    parser.object( mynetwork );
+  }
+
+  cerr << "Number of layers: " << mynetwork.num_layers << "\n";
+
+  auto prng = get_random_engine();
+  auto tempo_distribution = uniform_real_distribution<float>( 30, 240 ); // beats per minute
+  auto noise_distribution = normal_distribution<float>( 0, note_timing_variation );
+
+  const auto input_generator = [&]( DNN::M_input<1>& sample_input, DNN::M_output<1>& target_output ) {
+    const float tempo = tempo_distribution( prng );
+    target_output( 0, 0 ) = tempo;
+
+    const float seconds_per_beat = 60.0 / tempo;
+
+    auto offset_distribution = uniform_real_distribution<float>( 0, seconds_per_beat );
+    const float offset = offset_distribution( prng );
+
+    for ( unsigned int note_num = 0; note_num < 16; note_num++ ) {
+      sample_input( note_num ) = offset + seconds_per_beat * note_num * ( 1 + noise_distribution( prng ) );
+    }
+  };
+
+  const auto output_transformer = []( const auto& singleton, float& out ) { out = singleton( 0, 0 ); };
+
+  /* listen for connections */
   TCPSocket server_socket {};
   server_socket.set_reuseaddr();
 
@@ -43,7 +84,8 @@ int main()
     [&] { client_buffer.push_from_fd( connection ); },
     [&] { return not client_buffer.writable_region().empty(); } );
 
-  const string response_str = "<span style='color:blue;'>Hello, everybody. Time for lunch!</span>";
+  using MySampler = Sampler<64, DNN, float>;
+  MySampler::Output outputs;
 
   events.add_rule(
     "Parse bytes from client buffer",
@@ -54,9 +96,16 @@ int main()
         the_response.http_version = "HTTP/1.1";
         the_response.status_code = "200";
         the_response.reason_phrase = "OK";
-        the_response.headers.content_type = "text/html";
-        the_response.headers.content_length = response_str.size();
-        the_response.body = response_str;
+        the_response.headers.content_type = "image/svg+xml";
+
+        MySampler::sample( 16, mynetwork, input_generator, output_transformer, outputs );
+
+        Graph graph { { 640, 480 }, { 0, 270 }, { 0, 270 } };
+        graph.graph( outputs );
+        graph.finish();
+
+        the_response.headers.content_length = graph.svg().size();
+        the_response.body = move( graph.svg() );
         http_server.push_response( move( the_response ) );
       }
     },
@@ -78,6 +127,25 @@ int main()
   }
 
   cerr << "Exiting...\n";
+}
 
-  return 0;
+int main( int argc, char* argv[] )
+{
+  if ( argc < 0 ) {
+    abort();
+  }
+
+  if ( argc != 2 ) {
+    cerr << "Usage: " << argv[0] << " filename\n";
+    return EXIT_FAILURE;
+  }
+
+  try {
+    program_body( argv[1] );
+  } catch ( const exception& e ) {
+    cerr << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
