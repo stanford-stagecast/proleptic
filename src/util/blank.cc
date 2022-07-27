00c8cf8d018f05736562e058b0d6bfc2749afe97
Keith Winstein <keithw@cs.stanford.edu>
2022-07-27 12:04:56 -0700

    Import HTTP and Socket implementations

--- src/util/blank.cc
+++
@@ -1,2 +0,0 @@
-#include "parser.hh"
-/* XXX stop cmake from complaining */
---
+++ src/util/http_structures.hh
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <optional>
+#include <string>
+
+struct HTTPHeaders
+{
+  std::optional<size_t> content_length {};
+  std::string host {};
+  std::string connection {};
+  std::string upgrade {};
+  std::string origin {};
+  std::string sec_websocket_key {}, sec_websocket_accept {};
+};
+
+struct HTTPRequest
+{
+  std::string method {}, request_target {}, http_version {};
+  HTTPHeaders headers {};
+  std::string body {};
+};
+
+struct HTTPResponse
+{
+  std::string http_version {}, status_code {}, reason_phrase {};
+  HTTPHeaders headers {};
+  std::string body {};
+};
