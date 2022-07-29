633f3ebb55756d23e0d33a02ea9a223471c1c6ef
Jonathan Coronado <jcoronad@puffer5.stanford.edu>
2022-07-29 11:57:24 -0700

    Added test to verify roundtrip robustness of serialization and deserialization

--- src/nn/serdes.cc
+++ src/nn/serdes.cc
@@ -10,6 +10,19 @@ static constexpr string_view layer_header_str_view { layer_header_str.data(), la
 
 static constexpr string_view network_header_str_view { network_header_str.data(), network_header_str.size() };
 
+static constexpr array<char, 8> test_input_header_str { 'i', 'n', 'p', 'u', 't', ' ', ' ', ' ' };
+static constexpr array<char, 8> test_output_header_str { 'o', 'u', 't', 'p', 'u', 't', ' ', ' ' };
+static constexpr array<char, 8> roundtrip_test_end_str { 's', 't', 'o', 'p', 'h', 'e', 'r', 'e' };
+
+static constexpr string_view test_input_header_str_view { test_input_header_str.data(),
+                                                          test_input_header_str.size() };
+
+static constexpr string_view test_output_header_str_view { test_output_header_str.data(),
+                                                           test_output_header_str.size() };
+
+static constexpr string_view roundtrip_test_end_str_view { roundtrip_test_end_str.data(),
+                                                           roundtrip_test_end_str.size() };
+
 namespace LayerSerDes {
 template<typename T_layer>
 void serialize( const T_layer& layer, Serializer& out )
@@ -86,11 +99,38 @@ void serialize( const T_network& network, Serializer& out )
 
   serialize_internal( network, out );
 
-  /* todo:
+  /* Consistency check for serialization and parsing 
      (1) generate 16 random inputs (type T_network::M_input)
      (2) apply each one through the network to produce an output
      (3) serialize each input (maybe with an "input   " header)
          and its corresponding output (maybe with an "output  " header) */
+
+  // initialize some random input
+  typename T_network::template M_input<16> my_input;
+  my_input.Random();
+
+  // initialize output container
+  typename T_network::template Activations<16> activations;
+  network.apply( my_input, activations );
+
+  // serialize input header
+  out.string( test_input_header_str_view );
+
+  // serialize input
+  for ( unsigned int i = 0; i < my_input.size(); ++i ) {
+    out.floating( *( my_input.data() + i ) );
+  }
+
+  // serialize output header
+  out.string( test_output_header_str_view );
+
+  // serialize output
+  for ( unsigned int i = 0; i < activations.output().size(); ++i ) {
+    out.floating( *( activations.output().data() + i ) );
+  }
+
+  // serialize end of test
+  out.string( roundtrip_test_end_str_view );
 }
 
 template<typename T_network>
@@ -105,7 +145,7 @@ void parse( T_network& network, Parser& in )
 
   parse_internal( network, in );
 
-  /* todo:
+  /* Consistency check for serialization and parsing 
      (1) parse each of the 16 random inputs (each starting with the
          "input   " header)
      (2) parse each of the 16 corresponding outputs (each starting
@@ -114,6 +154,50 @@ void parse( T_network& network, Parser& in )
      (4) confirm that the computed output matches the parsed expectation
      (by "matched" maybe we mean it's within .01% or something -- doesn't
      have to be perfectly exact in the == sense between two floats) */
+
+  // input header demarcates input
+  array<char, 8> test_input_header;
+  string_span test_input_header_span { test_input_header.data(), test_input_header.size() };
+
+  // verify input header
+  in.string( test_input_header_span );
+  if ( test_input_header != test_input_header_str ) {
+    throw runtime_error( "missing test input header " + string( test_input_header_span ) );
+  }
+
+  // read in input
+  typename T_network::template M_input<16> my_input;
+
+  for ( unsigned int i = 0; i < my_input.size(); ++i ) {
+    in.floating( *( my_input.data() + i ) );
+  }
+
+  // output header demarcates output
+  array<char, 8> test_output_header;
+  string_span test_output_header_span { test_output_header.data(), test_output_header.size() };
+
+  // verify output header
+  in.string( test_output_header_span );
+  if ( test_output_header != test_output_header_str ) {
+    throw runtime_error( "missing test output header" );
+  }
+
+  // read in output
+  typename T_network::template Activations<16> activations;
+
+  for ( unsigned int i = 0; i < activations.output().size(); ++i ) {
+    in.floating( *( activations.output().data() + i ) );
+  }
+
+  // read in end of test
+  array<char, 8> test_end_point;
+  string_span test_end_point_span { test_end_point.data(), test_end_point.size() };
+
+  // verify end of test
+  in.string( test_end_point_span );
+  if ( test_end_point != roundtrip_test_end_str ) {
+    throw runtime_error( "mismatched roundtrip test ends" );
+  }
 }
 }
 
