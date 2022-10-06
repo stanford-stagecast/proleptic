c45c7b356e9b4fa6cd6843462dded0cd43dabc62
Akshay Srivatsan <srivatsan@cs.stanford.edu>
2022-10-06 16:33:21 -0700

    Disable testing and formatting when not top-level repo

--- etc/tests.cmake
+++ etc/tests.cmake
@@ -1,6 +1,6 @@
 enable_testing ()
 
-add_custom_target(check-simplenn COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --timeout 10 -R '^t_')
+add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --timeout 10 -R '^t_')
 
 add_test(NAME t_serdes_roundtrip COMMAND serdes-roundtrip)
 add_test(NAME t_gradient_descent COMMAND gradient-descent)
