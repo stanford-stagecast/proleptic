18b71c563ac04d339ec9cca14099b80bcf2ffba9
Keith Winstein <keithw@cs.stanford.edu>
2022-10-07 00:13:59 -0700

    Refactor ex3 to use WebServer class

--- src/ex3.cc
+++ src/ex3.cc
@@ -3,7 +3,6 @@
 #include "eventloop.hh"
 #include "exception.hh"
 #include "grapher.hh"
-#include "http_server.hh"
 #include "inference.hh"
 #include "mmap.hh"
 #include "network.hh"
@@ -11,13 +10,12 @@
 #include "parser.hh"
 #include "random.hh"
 #include "serdes.hh"
-#include "socket.hh"
+#include "web_server.hh"
 
 #include <iostream>
 
 using namespace std;
 
-static constexpr size_t mebi = 1024 * 1024;
 static constexpr float note_timing_variation = 0.05; /* jitter = stddev of +/- 5% of the beat */
 
 static constexpr size_t num_batches = 8;
@@ -89,107 +87,61 @@ void program_body( const string& filename )
 
   vector<pair<float, float>> target_actual_inference;
 
-  /* listen for connections */
-  TCPSocket server_socket {};
-  server_socket.set_reuseaddr();
-
-  try {
-    server_socket.bind( { "0", 9090 } );
-  } catch ( ... ) {
-    server_socket.bind( { "0", 0 } );
-  }
-
-  server_socket.listen();
-  cerr << "Listening on port " << server_socket.local_address().port() << "\n";
-
-  EventLoop events;
-
-  auto connection = server_socket.accept();
-  cerr << "Connection received from " << connection.peer_address().to_string() << "\n";
-
-  RingBuffer client_buffer { mebi }, server_buffer { mebi };
-
-  HTTPServer http_server;
-  HTTPRequest request;
-
-  events.add_rule(
-    "Read bytes from client",
-    connection,
-    Direction::In,
-    [&] { client_buffer.push_from_fd( connection ); },
-    [&] { return not client_buffer.writable_region().empty(); } );
-
   const pair<float, float> init { numeric_limits<float>::max(), 0 };
   vector<pair<float, float>> weight_range( mynetwork.num_layers, init );
   vector<pair<float, float>> bias_range( mynetwork.num_layers, init );
   vector<pair<float, float>> activation_range( mynetwork.num_layers, init );
 
-  events.add_rule(
-    "Parse bytes from client buffer",
-    [&] {
-      if ( http_server.read( client_buffer, request ) ) {
-        HTTPResponse the_response;
-        the_response.http_version = "HTTP/1.1";
-        the_response.status_code = "200";
-        the_response.reason_phrase = "OK";
+  EventLoop events;
 
-        if ( request.request_target == "/" ) {
-          the_response.headers.content_type = "text/html";
-          the_response.body = html_page;
-        } else {
-          the_response.headers.content_type = "image/svg+xml";
+  WebServer web_server_ {
+    events, 12345, [&]( const HTTPRequest& request, HTTPResponse& the_response ) {
+      the_response.http_version = "HTTP/1.1";
+      the_response.status_code = "200";
+      the_response.reason_phrase = "OK";
 
-          target_actual_inference.clear();
-          graph.reset_activations();
+      if ( request.request_target == "/" ) {
+        the_response.headers.content_type = "text/html";
+        the_response.body = html_page;
+      } else {
+        the_response.headers.content_type = "image/svg+xml";
 
-          for ( size_t batch = 0; batch < num_batches; ++batch ) {
-            // step 1: generate one batch of input
-            for ( size_t elem = 0; elem < BATCH_SIZE; ++elem ) {
-              input_generator( single_input, single_output );
-              input_batch.row( elem ) = single_input;
-              target_actual_inference.emplace_back();
-              output_transformer( single_output, target_actual_inference.at( batch * BATCH_SIZE + elem ).first );
-            }
+        target_actual_inference.clear();
+        graph.reset_activations();
 
-            // step 2: apply the network to the batch
-            inference.apply( mynetwork, input_batch );
-
-            // step 3: grab the actual inference outputs
-            for ( unsigned int elem = 0; elem < BATCH_SIZE; ++elem ) {
-              output_transformer( inference.output().row( elem ),
-                                  target_actual_inference.at( batch * BATCH_SIZE + elem ).second );
-            }
-
-            // step 4: grab all the activations
-            graph.add_activations( inference );
+        for ( size_t batch = 0; batch < num_batches; ++batch ) {
+          // step 1: generate one batch of input
+          for ( size_t elem = 0; elem < BATCH_SIZE; ++elem ) {
+            input_generator( single_input, single_output );
+            input_batch.row( elem ) = single_input;
+            target_actual_inference.emplace_back();
+            output_transformer( single_output, target_actual_inference.at( batch * BATCH_SIZE + elem ).first );
           }
 
-          the_response.body.append( graph.io_graph( target_actual_inference, "Target vs. Inferred", "bpm" ) );
-          the_response.body.append( "<p>" );
+          // step 2: apply the network to the batch
+          inference.apply( mynetwork, input_batch );
 
-          auto layer_graphs = graph.layer_graphs();
-          for ( const auto& layer : layer_graphs ) {
-            the_response.body.append( layer );
-            the_response.body.append( "<p>" );
+          // step 3: grab the actual inference outputs
+          for ( unsigned int elem = 0; elem < BATCH_SIZE; ++elem ) {
+            output_transformer( inference.output().row( elem ),
+                                target_actual_inference.at( batch * BATCH_SIZE + elem ).second );
           }
+
+          // step 4: grab all the activations
+          graph.add_activations( inference );
         }
-        the_response.headers.content_length = the_response.body.size();
-        http_server.push_response( move( the_response ) );
+
+        the_response.body.append( graph.io_graph( target_actual_inference, "Target vs. Inferred", "bpm" ) );
+        the_response.body.append( "<p>" );
+
+        auto layer_graphs = graph.layer_graphs();
+        for ( const auto& layer : layer_graphs ) {
+          the_response.body.append( layer );
+          the_response.body.append( "<p>" );
+        }
       }
-    },
-    [&] { return not client_buffer.readable_region().empty(); } );
-
-  events.add_rule(
-    "Serialize bytes from server into server buffer",
-    [&] { http_server.write( server_buffer ); },
-    [&] { return not http_server.responses_empty() and not server_buffer.writable_region().empty(); } );
-
-  events.add_rule(
-    "Write bytes to client",
-    connection,
-    Direction::Out,
-    [&] { server_buffer.pop_to_fd( connection ); },
-    [&] { return not server_buffer.readable_region().empty(); } );
+      the_response.headers.content_length = the_response.body.size();
+    } };
 
   while ( events.wait_next_event( -1 ) != EventLoop::Result::Exit ) {}
 
