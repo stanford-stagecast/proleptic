203a2c153e7dcad8a462a54be20dc591c7826153
Akshay Srivatsan <srivatsan@cs.stanford.edu>
2022-10-13 16:52:25 -0700

    Update the rhythm demo with the pattern of 'Twinkle Twinkle Little Star'

--- src/rhythm-demo.cc
+++ src/rhythm-demo.cc
@@ -28,29 +28,33 @@ const float LEARNING_RATE = 0.15;
 
 // Assume one interval = one sixteenth note
 const vector<int> HOT_CROSS_BUNS = { 4, 4, 8, 4, 4, 8, 2, 2, 2, 2, 2, 2, 2, 2, 4, 4, 8 };
+const vector<int> TWINKLE_TWINKLE_LITTLE_STAR = { 4, 4, 4, 4, 4, 4, 8, 4, 4, 4, 4, 4, 4, 8 };
 const int REPETITIONS = 10;
+const int NUM_SONGS = 2;
 
 void print_notes( const vector<int>& notes )
 {
   unsigned intervals = 0;
+  const int total_length = accumulate( notes.begin(), notes.end(), 0 );
+  const int one_repetition = total_length / REPETITIONS;
   for ( unsigned i = 0; i < notes.size(); i++ ) {
     int note = notes[i];
     intervals += note;
     switch ( note ) {
       case 1:
-        cout << "𝅘𝅥𝅯 ";
+        cout << "𝅘𝅥𝅯";
         break;
       case 2:
-        cout << "𝅘𝅥𝅮  ";
+        cout << "𝅘𝅥𝅮 ";
         break;
       case 4:
-        cout << "𝅘𝅥 ";
+        cout << "𝅘𝅥   ";
         break;
       case 8:
-        cout << "𝅗𝅥 ";
+        cout << "𝅗𝅥       ";
         break;
       case 16:
-        cout << "𝅝 ";
+        cout << "𝅝               ";
         break;
       default:
         cout << note << " ";
@@ -59,9 +63,9 @@ void print_notes( const vector<int>& notes )
     if ( intervals == 0 || i == notes.size() - 1 )
       continue;
     if ( intervals % 16 == 0 ) {
-      cout << "| ";
+      /* cout << "|"; */
     }
-    if ( ( i + 1 ) % ( notes.size() / REPETITIONS ) == 0 ) {
+    if ( ( intervals % one_repetition ) == 0 ) {
       cout << endl;
     }
   }
@@ -91,7 +95,10 @@ void program_body( const string& filename )
   }
 
   vector<int> notes;
-  for ( unsigned i = 0; i < REPETITIONS; i++ ) {
+  for ( unsigned i = 0; i < REPETITIONS / NUM_SONGS; i++ ) {
+    notes.insert( notes.end(), TWINKLE_TWINKLE_LITTLE_STAR.begin(), TWINKLE_TWINKLE_LITTLE_STAR.end() );
+  }
+  for ( unsigned i = 0; i < REPETITIONS / NUM_SONGS; i++ ) {
     notes.insert( notes.end(), HOT_CROSS_BUNS.begin(), HOT_CROSS_BUNS.end() );
   }
   vector<int> piano_roll;
@@ -103,11 +110,11 @@ void program_body( const string& filename )
     }
   }
 
-  cout << "Hot Cross Buns" << endl;
+  cout << "Hot Cross Buns + Twinkle Twinkle Little Star" << endl;
   cout << "Notes: " << endl;
   print_notes( notes );
-  cout << "True: " << endl;
-  print_piano_roll( piano_roll );
+  /* cout << "True: " << endl; */
+  /* print_piano_roll( piano_roll ); */
 
   Input input;
 
@@ -115,6 +122,7 @@ void program_body( const string& filename )
   unsigned total = 0;
 
   vector<int> played;
+  size_t iteration = 0;
   for ( unsigned i = 0; i < piano_roll.size(); i++ ) {
     if ( i < input.cols() ) {
       for ( unsigned j = 0; j < input.cols() - i; j++ ) {
@@ -147,27 +155,51 @@ void program_body( const string& filename )
       correct++;
     total++;
     if ( total == piano_roll.size() / REPETITIONS ) {
-      cout << "Repetition " << ( i * REPETITIONS ) / piano_roll.size() << ": " << correct << "/" << total << " = "
+      if ( iteration % ( REPETITIONS / NUM_SONGS ) == 0 ) {
+        cout << "New Song" << endl;
+      }
+      cout << "Iteration " << ( i * REPETITIONS ) / piano_roll.size() << ": " << correct << "/" << total << " = "
            << ( correct / (float)total * 100 ) << "%" << endl;
       correct = 0;
       total = 0;
+      iteration++;
     }
     played.push_back( note_predicted );
   }
 
-  cout << "Played: " << endl;
-  vector<int> played_notes;
-  int count = 0;
-  for ( unsigned i = 1; i < played.size(); i++ ) {
-    if ( played[i] == 1 ) {
-      played_notes.push_back( count + 1 );
-      count = 0;
-    } else {
-      count++;
+  cout << "Results: " << endl;
+
+  iteration = 0;
+  for ( size_t i = 0; i < played.size(); i++ ) {
+    const string green = "\033[32m";
+    const string red = "\033[31m";
+    const string reset = "\033[0m";
+    const string bold = "\033[1m";
+    const string dim = "\033[2m";
+    cout << ( piano_roll[i] == played[i] ? green : red );
+    cout << ( piano_roll[i] == 1 ? bold : dim );
+    cout << played[i];
+    cout << reset;
+    if ( ( i + 1 ) % ( played.size() / REPETITIONS ) == 0 ) {
+      cout << endl;
+      iteration++;
+      if ( ( iteration ) % ( REPETITIONS / NUM_SONGS ) == 0 ) {
+        cout << endl;
+      }
     }
   }
-  print_notes( played_notes );
-  print_piano_roll( played );
+  /* vector<int> played_notes; */
+  /* int count = 0; */
+  /* for ( unsigned i = 1; i < played.size(); i++ ) { */
+  /*   if ( played[i] == 1 ) { */
+  /*     played_notes.push_back( count + 1 ); */
+  /*     count = 0; */
+  /*   } else { */
+  /*     count++; */
+  /*   } */
+  /* } */
+  /* print_notes( played_notes ); */
+  /* print_piano_roll( played ); */
 }
 
 int main( int argc, char* argv[] )
