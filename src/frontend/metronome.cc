127a1b47ef96570ab446e32fb31034be36bde093
Alex-q-z <qizhengz@uchicago.edu>
2022-11-01 11:48:24 -0700

    Fix oscillator to handle period prediction that is too large to be rendered

--- src/frontend/metronome.cc
+++ src/frontend/metronome.cc
@@ -13,14 +13,68 @@
 using namespace std;
 using namespace chrono;
 
-static constexpr unsigned int audio_horizon = 17; /* samples */
+const int input_size = 4;
+
+static constexpr unsigned int audio_horizon = input_size + 1; /* samples */
 static constexpr float max_amplitude = 0.9;
 static constexpr float note_decay_rate = 0.9998;
 static constexpr auto simulated_latency = milliseconds( 100 );
 
 // static string nn_file = "/usr/local/share/predict-period.dnn";
 static string nn_file
-  = "/home/qizhengz/stagecast/simplenn-1031/models/period-from-pattern-sixteenth-with-missing-no-dotted-eighth.dnn";
+  = "/home/qizhengz/stagecast/models_new/period-from-pattern-sixteenth-with-missing-no-dotted-eighth.dnn";
+// static string nn_file = "/home/qizhengz/stagecast/simplenn-1031/predict-period-from-pattern-new.dnn";
+// static string nn_file = "/home/qizhengz/stagecast/models_new/period-from-midi-quarter-with-missing.dnn";
+
+float largest_element_in_array( array<float, input_size> my_array )
+{
+  float largest_element = -1.0;
+  for ( float element : my_array ) {
+    if ( element > largest_element )
+      largest_element = element;
+  }
+  return largest_element;
+}
+
+float smallest_element_in_array( array<float, input_size> my_array )
+{
+  float smallest_element = 50.0;
+  for ( float element : my_array ) {
+    if ( element < smallest_element )
+      smallest_element = element;
+  }
+  return smallest_element;
+}
+
+float compute_period_rule_based( array<float, input_size> timestamp_deltas )
+{
+
+  float smallest_interval = 100.0;
+  for ( size_t i = 0; i < input_size; i++ ) {
+    if ( timestamp_deltas[i] < smallest_interval and timestamp_deltas[i] > 0.01 ) {
+      smallest_interval = timestamp_deltas[i];
+    }
+  }
+  if ( smallest_interval == 100.0 )
+    smallest_interval = 0.0;
+
+  float average_interval = 0.0;
+  size_t count = 0;
+
+  for ( size_t i = 0; i < input_size; i++ ) {
+    for ( int x = 1; x < 6; x++ ) {
+      if ( timestamp_deltas[i] > 0.01
+           and abs( timestamp_deltas[i] / (float)x - smallest_interval ) / smallest_interval < 0.1 ) {
+        average_interval += timestamp_deltas[i] / x;
+        count++;
+      }
+    }
+  }
+  if ( count != 0 )
+    average_interval /= count;
+
+  return average_interval;
+}
 
 float compute_phase_sixteenth( float first_timestamp, float period )
 {
@@ -40,9 +94,35 @@ float compute_phase_quarter( float first_timestamp, float period )
   return ( first_timestamp / quarter_note_interval ) * 2 * M_PI;
 }
 
-array<float, 17> calculate_input( deque<steady_clock::time_point> times )
+// this computes phase based on an external time-clock instead of the last timestamp
+float compute_phase_based_on_clock( steady_clock::time_point current_time,
+                                    float current_period,
+                                    steady_clock::time_point last_time,
+                                    float last_period,
+                                    float last_phase )
 {
-  array<float, 17> ret_mat {};
+  float duration = duration_cast<milliseconds>( current_time - last_time ).count() / 1000.f;
+  // cout << "duration = " << duration << endl;
+  float current_phase = 0;
+
+  if ( duration < last_period ) {
+    current_phase = last_phase + duration / last_period * 2 * M_PI;
+    while ( current_phase > 2 * M_PI )
+      current_phase -= 2 * M_PI;
+  } else {
+    float time_in_period = duration;
+    while ( time_in_period > current_period ) {
+      time_in_period -= last_period;
+    }
+    current_phase = time_in_period / current_period * 2 * M_PI;
+  }
+
+  return current_phase;
+}
+
+array<float, input_size + 1> calculate_input( deque<steady_clock::time_point> times )
+{
+  array<float, input_size + 1> ret_mat {};
   deque<float> timestamps;
   steady_clock::time_point current_time = steady_clock::now();
   for ( const auto& time : times ) {
@@ -53,7 +133,7 @@ array<float, 17> calculate_input( deque<steady_clock::time_point> times )
   }
   reverse( begin( timestamps ), end( timestamps ) );
 
-  for ( size_t i = 0; i < 17; i++ ) {
+  for ( size_t i = 0; i < input_size + 1; i++ ) {
     if ( i >= timestamps.size() ) {
       ret_mat[i] = 0;
     } else {
@@ -79,12 +159,12 @@ void program_body( const string_view audio_device, const string& midi_device )
   const auto device_claim = AudioDeviceClaim::try_claim( name );
 
   /* use ALSA to initialize and configure audio device */
-  const auto short_name = audio_device.substr( 0, 17 );
+  const auto short_name = audio_device.substr( 0, input_size + 1 );
   auto playback_interface = make_shared<AudioInterface>( interface_name, short_name, SND_PCM_STREAM_PLAYBACK );
   AudioInterface::Configuration config;
   config.sample_rate = 48000;           /* samples per second */
   config.buffer_size = 48;              /* maximum samples of queued audio = 1 millisecond */
-  config.period_size = 17;              /* chunk size for kernel's management of audio buffer */
+  config.period_size = 16;              /* chunk size for kernel's management of audio buffer */
   config.avail_minimum = audio_horizon; /* device is writeable when full horizon can be written */
   long microseconds_per_samp = static_cast<long>( 1000000 / double( config.sample_rate ) + 0.5 );
   playback_interface->set_config( config );
@@ -109,10 +189,14 @@ void program_body( const string_view audio_device, const string& midi_device )
   // SimpleNN nn { nn_file };
   PeriodPredictor nn { nn_file };
   float time_since_pred_note = 0;
-  array<float, 17> past_timestamps {};
-  array<float, 16> timestamp_deltas {};
+  array<float, input_size + 1> past_timestamps {};
+  array<float, input_size> timestamp_deltas {};
   size_t counter = 0;
   float last_period = 0.0;
+
+  steady_clock::time_point checkpoint_time { steady_clock::now() };
+  float checkpoint_period;
+  float checkpoint_phase;
 
   /* rule #1: write a continuous sine wave (but no more than 1.3 ms into the future) */
   event_loop->add_rule(
@@ -178,7 +262,7 @@ void program_body( const string_view audio_device, const string& midi_device )
         if ( midi.get_event_type() == 144 ) { /* key down */
           amp_left = max_amplitude;
           // keep a few extra in case some of the new presses are discarded to simulate latency
-          if ( num_notes < 17 + 4 ) {
+          if ( num_notes < input_size + 1 + 4 ) {
             press_queue.push_back( time_val );
             num_notes++;
           } else {
@@ -199,7 +283,7 @@ void program_body( const string_view audio_device, const string& midi_device )
       past_timestamps = calculate_input( press_queue );
 
       // process timestamps to get the deltas
-      for ( int k = 0; k < 16; ++k ) {
+      for ( int k = 0; k < input_size; ++k ) {
         if ( past_timestamps[k] == 0 || past_timestamps[k + 1] == 0 )
           timestamp_deltas[k] = 0;
         else
@@ -207,35 +291,76 @@ void program_body( const string_view audio_device, const string& midi_device )
       }
 
       // predict period
-      float period = nn.predict_period( timestamp_deltas );
+      // float period = nn.predict_period( timestamp_deltas );
+      float period = compute_period_rule_based( timestamp_deltas );
       cerr << "timestamp_deltas = ";
-      for ( int i = 0; i < 16; i++ ) {
+      for ( int i = 0; i < input_size; i++ ) {
         cerr << timestamp_deltas[i] << " ";
       }
       cerr << "\n";
       cerr << "predicted period = " << period << "\n";
 
       // a very simple oscillator
-      if ( last_period != 0.0 ) {
+      if ( period != 0.0 and last_period != 0.0 ) {
+        // check if period increases or decreases by a factor
+        // if so, adjust the rendered period so that it is not oscillating
         for ( int x = 2; x < 6; x++ ) {
           if ( abs( period / x - last_period ) / last_period < 0.1 ) {
             period = last_period;
             break;
+          }
+          if ( abs( period * x - last_period ) / last_period < 0.1 ) {
+            period = last_period;
+            break;
+          }
+        }
+
+        // check if period is oscillating slightly in a range
+        // if so, adjust the rendered period so that it is not oscillating
+        if ( abs( period - last_period ) / ( last_period ) < 0.15 ) {
+          period = last_period;
+        }
+
+        // manually cap period in a certain reasonable range
+        while ( period < 0.3 or period > 1.0 ) {
+          if ( period < 0.3 ) {
+            period *= 2;
+          } else {
+            period /= 2;
           }
         }
       }
       last_period = period;
       cerr << "adjusted period = " << period << "\n";
 
-      // float phase = compute_phase_sixteenth( past_timestamps[0], period );
-      float phase = compute_phase_quarter( past_timestamps[0], period );
-      float time_to_next = ( 2 * M_PI - phase ) / ( 2 * M_PI ) * period * ( -1 );
+      // compute phase
+      float phase;
+      if ( period == 0.0 ) {
+        phase = 0.0;
+      } else if ( checkpoint_period == 0.0 and checkpoint_phase == 0.0 )
+        phase = compute_phase_quarter( past_timestamps[0], period );
+      else {
+        steady_clock::time_point phase_current_time { steady_clock::now() };
+        phase = compute_phase_based_on_clock(
+          phase_current_time, period, checkpoint_time, checkpoint_period, checkpoint_phase );
+      }
+      cout << "phase = " << phase << endl;
 
-      // the "future" is actually based on a moment `simulated_latency` in the past
+      float time_to_next = ( 2 * M_PI - phase ) / ( 2 * M_PI ) * period * ( -1 );
       time_to_next += ( duration_cast<milliseconds>( simulated_latency ) ).count() / 1000.f;
-      cerr << "time to next = " << time_to_next << "\n";
-      counter++;
-      next_note_pred = last_pred_time - round<milliseconds>( duration<float> { time_to_next } );
+
+      // save the checkpoint info for period and phase at this time
+      checkpoint_time = steady_clock::now();
+      checkpoint_period = period;
+      checkpoint_phase = phase;
+
+      // render metromone audio only when the latest beat has not been rendered
+      if ( next_note_pred < steady_clock::now() ) {
+        // the "future" is actually based on a moment `simulated_latency` in the past
+        cerr << "time to next = " << time_to_next << "\n";
+        counter++;
+        next_note_pred = last_pred_time - round<milliseconds>( duration<float> { time_to_next } );
+      }
     },
     [&] { return duration_cast<milliseconds>( steady_clock::now() - last_pred_time ).count() >= 50; } );
 
