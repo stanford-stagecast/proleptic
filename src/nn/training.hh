929034ebace1a3da4b9274949882065d28e26fe4
Akshay Srivatsan <srivatsan@cs.stanford.edu>
2022-11-01 11:57:16 -0700

    Add a training function which backs off

--- src/nn/training.hh
+++ src/nn/training.hh
@@ -3,6 +3,8 @@
 #include <Eigen/Dense>
 #include <concepts>
 #include <functional>
+#include <iostream>
+#include <memory>
 #include <type_traits>
 
 #include "gradient_descent.hh"
@@ -19,22 +21,46 @@ struct NetworkTraining
   using Input = typename Infer::Input;
   using Output = typename Infer::Output;
   using PdLossWrtOutputs = typename Infer::Output;
+  std::unique_ptr<Infer> infer = std::make_unique<Infer>();
+  std::unique_ptr<BackProp> backprop = std::make_unique<BackProp>();
+  std::unique_ptr<GradientDescent> gradient_descent = std::make_unique<GradientDescent>();
 
   void train( Network& nn,
               const Input& input,
               const std::function<PdLossWrtOutputs( const Output )>& pd_loss_wrt_outputs,
               float learning_rate )
   {
-    Infer infer;
-    infer.apply( nn, input );
+    infer->apply( nn, input );
 
-    Output output = infer.output();
+    Output& output = infer->output();
     PdLossWrtOutputs pd = pd_loss_wrt_outputs( output );
 
-    BackProp backprop;
-    backprop.differentiate( nn, input, infer, pd );
+    backprop->differentiate( nn, input, *infer, pd );
 
-    GradientDescent gradient_descent;
-    gradient_descent.update( nn, backprop, learning_rate );
+    gradient_descent->update( nn, *backprop, learning_rate );
+  }
+
+  size_t train_with_backoff( Network& nn,
+                             const Input& input,
+                             const std::function<PdLossWrtOutputs( const Output )>& pd_loss_wrt_outputs,
+                             float max_learning_rate )
+  {
+    std::unique_ptr<Network> nn_backup = make_unique<Network>( nn );
+    float learning_rate = max_learning_rate;
+    size_t i = 0;
+    infer->apply( nn, input );
+    Output initial_gradient = pd_loss_wrt_outputs( infer->output() );
+    Output gradient;
+    do {
+      nn = *nn_backup;
+      if ( i++ > 5 ) {
+        return i;
+      }
+      train( nn, input, pd_loss_wrt_outputs, learning_rate );
+      infer->apply( nn, input );
+      learning_rate /= 10;
+      gradient = pd_loss_wrt_outputs( infer->output() );
+    } while ( initial_gradient.dot( gradient ) < 0 );
+    return i;
   }
 };
