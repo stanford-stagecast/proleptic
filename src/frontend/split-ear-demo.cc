f4554adbf8872173e1abe71ce9ebbf98c552e887
Akshay Srivatsan <srivatsan@cs.stanford.edu>
2022-11-03 17:47:58 -0700

    Add full-piano split-ear program

--- src/frontend/split-ear-demo.cc
+++ src/frontend/split-ear-demo.cc
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <deque>
 #include <iostream>
+#include <random>
 
 #include "alsa_devices.hh"
 #include "audio_device_claim.hh"
@@ -18,31 +19,7 @@ static constexpr float max_amplitude = 0.9;
 static constexpr float note_decay_rate = 0.9998;
 static constexpr auto simulated_latency = milliseconds( 100 );
 
-static string nn_file = "/usr/local/share/predict-timestamps.dnn";
-
-array<float, 16> calculate_input( deque<steady_clock::time_point> times )
-{
-  array<float, 16> ret_mat {};
-  deque<float> timestamps;
-  steady_clock::time_point current_time = steady_clock::now();
-  for ( const auto& time : times ) {
-    if ( ( current_time - time ) < simulated_latency ) {
-      continue;
-    }
-    timestamps.push_back( duration_cast<milliseconds>( current_time - time - simulated_latency ).count() / 1000.f );
-  }
-  reverse( begin( timestamps ), end( timestamps ) );
-
-  for ( size_t i = 0; i < 16; i++ ) {
-    if ( i >= timestamps.size() ) {
-      ret_mat[i] = 0;
-    } else {
-      ret_mat[i] = timestamps[i];
-    }
-  }
-
-  return ret_mat;
-}
+static string predictor_file = "/usr/local/share/piano-roll-predictor.dnn";
 
 void program_body( const string_view audio_device, const string& midi_device )
 {
@@ -63,7 +40,7 @@ void program_body( const string_view audio_device, const string& midi_device )
   auto playback_interface = make_shared<AudioInterface>( interface_name, short_name, SND_PCM_STREAM_PLAYBACK );
   AudioInterface::Configuration config;
   config.sample_rate = 48000;           /* samples per second */
-  config.buffer_size = 48;              /* maximum samples of queued audio = 1 millisecond */
+  config.buffer_size = 48 * 20;         /* maximum samples of queued audio = 1 millisecond */
   config.period_size = 16;              /* chunk size for kernel's management of audio buffer */
   config.avail_minimum = audio_horizon; /* device is writeable when full horizon can be written */
   long microseconds_per_samp = static_cast<long>( 1000000 / double( config.sample_rate ) + 0.5 );
@@ -73,23 +50,41 @@ void program_body( const string_view audio_device, const string& midi_device )
   /* open the MIDI device */
   FileDescriptor piano { CheckSystemCall( midi_device, open( midi_device.c_str(), O_RDONLY ) ) };
   MidiProcessor midi;
-  deque<steady_clock::time_point> press_queue {};
-  size_t num_notes = 0;
+  struct Press
+  {
+    steady_clock::time_point time;
+    int key;
+    int velocity;
+    Press( steady_clock::time_point t, int k, int v )
+      : time( t )
+      , key( k )
+      , velocity( v )
+    {}
+  };
+  deque<Press> pending_queue {};
+  deque<Press> history_queue {};
+  deque<array<bool, 88>> piano_roll { {} };
+  for ( size_t i = 0; i < 88; i++ ) {
+    piano_roll.back()[i] = PianoRollEvent::Unknown;
+  }
 
   /* get ready to play an audio signal */
   ChannelPair audio_signal { 16384 };  // the output signal
   size_t next_sample_to_calculate = 0; // what's the next sample # to be written to the output signal?
 
   /* current amplitude of the sine waves */
-  float amp_left = 0, amp_right = 0;
+  array<float, 88> amp_left = {}, amp_right = {};
 
+  steady_clock::time_point last_note_pred { steady_clock::now() };
   steady_clock::time_point next_note_pred { steady_clock::now() };
   steady_clock::time_point last_pred_time { steady_clock::now() };
   steady_clock::time_point curr_time { steady_clock::now() };
-  SimpleNN nn { nn_file };
+  array<bool, 88> should_play_next_pred {};
+  steady_clock::duration next_note_duration = milliseconds( 1000 );
+  steady_clock::duration last_note_duration = milliseconds( 1000 );
+  bool seen_note = false;
   float time_since_pred_note = 0;
-  array<float, 16> past_timestamps {};
-  size_t counter = 0;
+  SimpleNN nn { predictor_file };
 
   /* rule #1: write a continuous sine wave (but no more than 1.3 ms into the future) */
   event_loop->add_rule(
@@ -99,17 +94,33 @@ void program_body( const string_view audio_device, const string& midi_device )
       while ( next_sample_to_calculate <= playback_interface->cursor() + audio_horizon ) {
         const double time = next_sample_to_calculate / double( config.sample_rate );
         /* compute the sine wave amplitude (middle A, 440 Hz) */
-        audio_signal.safe_set(
-          next_sample_to_calculate,
-          { amp_left * sin( 2 * M_PI * 440 * time ), amp_right * sin( 2 * M_PI * 440 * 1.5 * time ) } );
-        amp_left *= note_decay_rate;
+        const float a0 = 27.50;
+        float total_amp_left = 0, total_amp_right = 0;
+        for ( size_t i = 0; i < 88; i++ ) {
+          const float frequency = a0 * pow( 2.f, i / 12.f );
+          // const float frequency = 440.f;
+          total_amp_left += amp_left[i] * sin( 2 * M_PI * frequency * time );
+          total_amp_right += amp_right[i] * sin( 2 * M_PI * ( 2 * frequency ) * time );
+        }
+        total_amp_left *= 0.2;
+        total_amp_right *= 0.2;
+        if ( not seen_note )
+          total_amp_right = 0.0;
+        audio_signal.safe_set( next_sample_to_calculate, { total_amp_left, total_amp_right } );
+        for ( size_t i = 0; i < 88; i++ ) {
+          amp_left[i] *= note_decay_rate;
+        }
         // amp_right = some equation based on note_decay_rate and next_note_pred
-        if ( next_note_pred <= curr_time && ( curr_time - next_note_pred ) < simulated_latency ) {
-          time_since_pred_note
-            = ( config.sample_rate * duration_cast<microseconds>( curr_time - next_note_pred ).count() ) / 1000000;
-          amp_right = max_amplitude * pow( note_decay_rate, time_since_pred_note );
-        } else {
-          amp_right *= note_decay_rate;
+        for ( size_t i = 0; i < 88; i++ ) {
+          if ( should_play_next_pred[i] && next_note_pred <= curr_time
+               && ( curr_time - next_note_pred ) < simulated_latency ) {
+            time_since_pred_note
+              = ( config.sample_rate * duration_cast<microseconds>( curr_time - next_note_pred ).count() )
+                / 1000000.f;
+            amp_right[i] = max_amplitude * pow( note_decay_rate, time_since_pred_note );
+          } else {
+            amp_right[i] *= note_decay_rate;
+          }
         }
         next_sample_to_calculate++;
         curr_time += microseconds( microseconds_per_samp );
@@ -152,43 +163,99 @@ void program_body( const string_view audio_device, const string& midi_device )
     [&] {
       while ( midi.has_event() ) {
         steady_clock::time_point time_val = midi.get_event_time();
-        if ( midi.get_event_type() == 144 ) { /* key down */
-          amp_left = max_amplitude;
-          // keep a few extra in case some of the new presses are discarded to simulate latency
-          if ( num_notes < 16 + 4 ) {
-            press_queue.push_back( time_val );
-            num_notes++;
-          } else {
-            press_queue.push_back( time_val );
-            press_queue.pop_front();
+        if ( midi.get_event_type() == 144 || midi.get_event_type() == 128 ) { /* key down */
+          uint8_t note_val = midi.get_event_note();
+          uint8_t velocity_val = midi.get_event_type() == 128 ? 0 : midi.get_event_velocity();
+          if ( note_val >= 21 and note_val <= 108 ) {
+            ssize_t note_index = ( note_val - 21 );
+            amp_left[note_index] = velocity_val ? velocity_val / 127.f * max_amplitude : amp_left[note_index];
           }
+          pending_queue.push_back( Press( time_val, note_val, velocity_val ) );
         }
         midi.pop_event();
       }
     },
     [&] { return midi.has_event(); } );
 
+  /* rule #4.5: add latency to MIDI events */
+  event_loop->add_rule(
+    "simulate latency on MIDI events",
+    [&] {
+      history_queue.push_back( pending_queue.front() );
+      pending_queue.pop_front();
+      if ( history_queue.size() > 512 ) {
+        history_queue.pop_front();
+      }
+    },
+    [&] {
+      const auto now = steady_clock::now();
+      return !pending_queue.empty() and pending_queue.front().time < now - simulated_latency;
+    } );
+
   /* rule #5: get DNN prediction */
   event_loop->add_rule(
     "get DNN prediction",
     [&] {
-      last_pred_time = steady_clock::now();
-      past_timestamps = calculate_input( press_queue );
+      const auto now = steady_clock::now();
+      const auto adjusted_now = now - simulated_latency;
+      std::vector<SimpleNN::KeyPress> past_timestamps;
 
-      float time_to_next = nn.predict_next_timestamp( past_timestamps );
-      // the "future" is actually based on a moment `simulated_latency` in the past
-      time_to_next += ( duration_cast<milliseconds>( simulated_latency ) ).count() / 1000.f;
-      if ( counter % 20 == 0 ) {
-        for ( int i = 0; i < 16; i++ ) {
-          cerr << past_timestamps[i] << " ";
+      for ( const auto& press : history_queue ) {
+        if ( press.velocity > 0 and press.key >= 21 and press.key <= 108 ) {
+          past_timestamps.push_back( SimpleNN::KeyPress( press.key - 21, press.time ) );
+          seen_note = true;
         }
-        cerr << '\n';
-        cerr << "Prediction: " << time_to_next << '\n';
       }
-      counter++;
-      next_note_pred = last_pred_time - round<milliseconds>( duration<float> { time_to_next } );
+
+      if ( next_note_pred < adjusted_now ) {
+        last_note_pred = next_note_pred;
+        const auto prediction = nn.predict_next_timeslot( past_timestamps );
+        next_note_pred = prediction.start;
+        last_note_duration = next_note_duration;
+        next_note_duration = prediction.length;
+        piano_roll.push_back( piano_roll.back() );
+        if ( piano_roll.size() > SimpleNN::HISTORY + 1 ) {
+          piano_roll.pop_front();
+        }
+      }
+
+      for ( const auto& press : history_queue ) {
+        if ( press.key < 21 or press.key > 108 )
+          continue;
+        if ( press.time > last_note_pred - last_note_duration / 2
+             && press.time <= next_note_pred - next_note_duration / 2 ) {
+          piano_roll.back()[press.key - 21] = press.velocity > 0 ? true : false;
+        }
+      }
+
+      if ( piano_roll.size() < 2 ) {
+        for ( size_t i = 0; i < 88; i++ ) {
+          should_play_next_pred[i] = false;
+        }
+        last_pred_time = now;
+        return;
+      }
+
+      vector<array<bool, 88>> old_notes( piano_roll.begin(), piano_roll.end() - 1 );
+      nn.train_next_note_values( old_notes, piano_roll.back() );
+
+      vector<array<bool, 88>> notes( piano_roll.begin() + 1, piano_roll.end() );
+      // cerr << "\n";
+      for ( size_t j = 0; j < notes.size(); j++ ) {
+        for ( size_t i = 0; i < 88; i++ ) {
+          // cerr << (bool)notes[j][i];
+        }
+        // cerr << "\n";
+      }
+      should_play_next_pred = nn.predict_next_note_values( notes );
+      for ( size_t i = 0; i < 88; i++ ) {
+        // cerr << should_play_next_pred[i];
+      }
+      // cerr << endl;
+
+      last_pred_time = now;
     },
-    [&] { return duration_cast<milliseconds>( steady_clock::now() - last_pred_time ).count() >= 50; } );
+    [&] { return duration_cast<milliseconds>( steady_clock::now() - last_pred_time ).count() >= 25; } );
 
   /* add a task that prints statistics occasionally */
   StatsPrinterTask stats_printer { event_loop };
--- src/libsimplenn/simplenn.hh
+++ src/libsimplenn/simplenn.hh
@@ -1,37 +1,18 @@
 #pragma once
+
+#include "autoencoder.hh"
+#include "dnn_types.hh"
+#include "mmap.hh"
+#include "piano_roll.hh"
+#include "serdes.hh"
+#include "training.hh"
+
 #include <array>
 #include <chrono>
 #include <memory>
+#include <optional>
 #include <string_view>
 #include <vector>
-
-struct NetworkData;
-
-class SimpleNN
-{
-public:
-  //! Create a new instance of the network described by the specified file.
-  //!
-  //! \param[in] filename  A file containing a serialized neural network.
-  //!
-  //! \note  The network in the file has to match the network structure currently expected by libsimplenn, which
-  //! might change over time.
-  SimpleNN( const std::string& filename );
-  ~SimpleNN();
-
-  //! Make a single prediction about the next note timestamp.
-  //!
-  //! \param[in] past_timestamps A list of previous note timestamps, starting from the most recent (the lowest
-  //! value) and going up to the 16th most recent.  The values should be in seconds relative to the time when this
-  //! function is called, where positive values are in the past and negative values are in the future.
-  //!
-  //! \return The predicted next timestamp, in seconds relative to when this function was called.  Positive values
-  //! are in the past and negative values are in the future.
-  float predict_next_timestamp( const std::array<float, 16>& past_timestamps );
-
-private:
-  std::unique_ptr<NetworkData> data_ {};
-};
 
 struct PeriodPredictorData;
 
@@ -50,22 +31,202 @@ private:
   std::unique_ptr<PeriodPredictorData> data_ {};
 };
 
-struct PianoRollPredictorData;
-
-class PianoRollPredictor
+class SimpleNN
 {
-public:
-  PianoRollPredictor( const std::string& filename );
-  ~PianoRollPredictor();
-
-  std::pair<std::chrono::steady_clock::time_point, std::chrono::steady_clock::duration> predict_next_note_time(
-    const std::vector<std::chrono::steady_clock::time_point>& timestamps );
-
-  bool predict_next_note_value( const std::vector<bool>& notes );
-  void train_next_note_value( const std::vector<bool>& notes, bool next );
-
 private:
-  std::unique_ptr<PianoRollPredictorData> data_ {};
+  using Predictor = DNN_piano_roll_prediction;
+  std::unique_ptr<Predictor> predictor_;
+
+public:
+  using Key = uint8_t;
+  using Time = std::chrono::steady_clock::time_point;
+  using Duration = std::chrono::steady_clock::duration;
+  struct KeyPress
+  {
+    Key key;
+    Time time;
+
+    KeyPress( Key k, Time t )
+      : key( k )
+      , time( t )
+    {}
+  };
+  struct Timeslot
+  {
+    Time start;
+    Duration length;
+
+    Timeslot( Time s, Duration l )
+      : start( s )
+      , length( l )
+    {}
+  };
+  using Column = std::array<bool, 88>;
+  using Roll = std::vector<Column>;
+  static constexpr size_t HISTORY = PIANO_ROLL_HISTORY;
+
+  SimpleNN( const std::string& predictor_file )
+    : predictor_( std::make_unique<Predictor>() )
+  {
+    {
+      ReadOnlyFile dnn_on_disk { predictor_file };
+      Parser parser { dnn_on_disk };
+      parser.object( *predictor_ );
+    }
+  }
+  ~SimpleNN() = default;
+
+  Timeslot predict_next_timeslot( const std::vector<KeyPress>& timestamps )
+  {
+    using namespace std;
+    using namespace chrono;
+    const auto now = steady_clock::now();
+    if ( timestamps.size() < 2 ) {
+      return Timeslot( now, milliseconds( 0 ) );
+    }
+    Duration smallest_interval( std::numeric_limits<long>::max() );
+
+    for ( size_t i = 1; i < min( timestamps.size(), (size_t)16 ); i++ ) {
+      auto candidate = timestamps[i].time - timestamps[i - 1].time;
+      bool different_keys = timestamps[i].key == timestamps[i - 1].key;
+      if ( different_keys and candidate < milliseconds( 100 ) )
+        continue;
+      if ( candidate < smallest_interval ) {
+        smallest_interval = candidate;
+      }
+    }
+
+    Duration average_interval { 0 };
+    size_t count = 0;
+
+    for ( size_t i = 1; i < min( timestamps.size(), (size_t)16 ); i++ ) {
+      auto candidate = timestamps[i].time - timestamps[i - 1].time;
+      for ( int x = 1; x < 6; x++ ) {
+        if ( abs( candidate.count() / (float)x - smallest_interval.count() ) / (float)smallest_interval.count()
+             < 0.1 ) {
+          average_interval += candidate / x;
+          count++;
+        }
+      }
+    }
+    average_interval /= count;
+
+    Time next_change = timestamps.back().time;
+
+    while ( next_change < now ) {
+      next_change += average_interval;
+    }
+
+    return Timeslot( next_change, average_interval );
+  }
+
+  Column predict_next_note_values( const Roll& roll )
+  {
+    using namespace std;
+    using Infer = NetworkInference<Predictor, 1>;
+
+    using Input = typename Infer::Input;
+    using Output = typename Infer::Output;
+
+    auto unknown = make_unique<Output>();
+    for ( ssize_t i = 0; i < unknown->size(); i++ ) {
+      ( *unknown )( i ) = PianoRollEvent::Unknown;
+    }
+    auto inputs = make_unique<std::vector<Output>>();
+
+    if ( roll.size() < HISTORY )
+      for ( size_t i = 0; i < HISTORY - roll.size(); i++ ) {
+        inputs->push_back( *unknown );
+      }
+
+    for ( const auto& column : roll ) {
+      Output current;
+      for ( size_t i = 0; i < column.size(); i++ ) {
+        current( i ) = column[i];
+      }
+      inputs->push_back( current );
+    }
+
+    auto combined_input = make_unique<Input>();
+    for ( size_t column = 0; column < inputs->size(); column++ ) {
+      for ( size_t row = 0; row < 88; row++ ) {
+        ( *combined_input )( row * HISTORY + column ) = ( *inputs )[column]( row );
+      }
+    }
+
+    auto infer = make_unique<Infer>();
+    infer->apply( *predictor_, *combined_input );
+
+    auto output = infer->output();
+
+    Column column;
+    for ( size_t i = 0; i < column.size(); i++ ) {
+      column[i] = output( i ) > 0.5;
+    }
+
+    return column;
+  }
+
+  void train_next_note_values( const std::vector<Column>& roll, const Column& next )
+  {
+    using namespace std;
+    using Infer = NetworkInference<Predictor, 1>;
+    using Train = NetworkTraining<Predictor, 1>;
+
+    using Input = typename Infer::Input;
+    using Output = typename Infer::Output;
+
+    auto unknown = make_unique<Output>();
+    for ( ssize_t i = 0; i < unknown->size(); i++ ) {
+      ( *unknown )( i ) = PianoRollEvent::Unknown;
+    }
+    auto inputs = make_unique<std::vector<Output>>();
+
+    if ( roll.size() < HISTORY )
+      for ( size_t i = 0; i < HISTORY - roll.size(); i++ ) {
+        inputs->push_back( *unknown );
+      }
+
+    for ( const auto& column : roll ) {
+      Output current;
+      for ( size_t i = 0; i < column.size(); i++ ) {
+        current( i ) = column[i];
+      }
+      inputs->push_back( current );
+    }
+
+    auto combined_input = make_unique<Input>();
+    for ( size_t column = 0; column < inputs->size(); column++ ) {
+      for ( size_t row = 0; row < 88; row++ ) {
+        ( *combined_input )( row * HISTORY + column ) = ( *inputs )[column]( row );
+      }
+    }
+
+    Output expected;
+    for ( size_t i = 0; i < next.size(); i++ ) {
+      expected( i ) = next[i];
+    }
+
+    auto train = make_unique<Train>();
+    train->train(
+      *predictor_,
+      *combined_input,
+      [&]( const auto& predicted ) {
+        auto sigmoid = []( const auto x ) { return 1.0 / ( 1.0 + exp( -x ) ); };
+        auto one_minus_x = []( const auto x ) { return 1.0 - x; };
+        auto sigmoid_prime = [&]( const auto x ) { return sigmoid( x ) * ( 1.0 - sigmoid( x ) ); };
+
+        Output sigmoid_predicted = predicted.unaryExpr( sigmoid );
+
+        Output pd
+          = ( -expected.cwiseQuotient( sigmoid_predicted )
+              + expected.unaryExpr( one_minus_x ).cwiseQuotient( sigmoid_predicted.unaryExpr( one_minus_x ) ) )
+              .cwiseProduct( predicted.unaryExpr( sigmoid_prime ) );
+
+        return pd;
+      },
+      0.01 );
+  }
 };
 
 struct OctavePredictorData;
