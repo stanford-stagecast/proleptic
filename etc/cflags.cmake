f07d384898cf81f8f374a839423d9e290ee21860
Alex-q-z <qizhengz@uchicago.edu>
2022-11-29 17:28:23 -0800

    Disable aggressive loop optimization for GCC 12.2

--- etc/cflags.cmake
+++ etc/cflags.cmake
@@ -2,7 +2,7 @@ set (CMAKE_CXX_STANDARD 20)
 set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
 set (CMAKE_BASE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 
-add_compile_options(-pedantic -pedantic-errors -Werror -Wall -Wextra -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Weffc++ -fno-signed-zeros -fno-math-errno -freciprocal-math -march=native -mtune=native -DEIGEN_STACK_ALLOCATION_LIMIT=0 -DEIGEN_NO_MALLOC -g)
+add_compile_options(-pedantic -pedantic-errors -Werror -Wall -Wextra -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Weffc++ -fno-signed-zeros -fno-math-errno -fno-aggressive-loop-optimizations -freciprocal-math -march=native -mtune=native -DEIGEN_STACK_ALLOCATION_LIMIT=0 -DEIGEN_NO_MALLOC -g)
 
 set (CMAKE_CXX_FLAGS_DEBUGASAN "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=undefined -fsanitize=address")
 set (CMAKE_CXX_FLAGS_RELASAN "${CMAKE_CXX_FLAGS_RELEASE} -fsanitize=undefined -fsanitize=address")
