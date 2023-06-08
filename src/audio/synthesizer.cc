531322d9aeb454a9ab36f4383154fe008ab230b2
Keith Winstein <keithw@cs.stanford.edu>
2023-06-07 18:17:24 -0700

    Update add_shallow_key_press to match endless ring buffer
    Doesn't sound perfect, but does sound semi-reasonable...

--- src/audio/synthesizer.cc
+++ src/audio/synthesizer.cc
@@ -71,32 +71,31 @@ void Synthesizer::add_shallow_key_press( uint8_t adj_event_note, uint8_t event_v
   k.released = false;
 
   NoteRepository::WavCombination combo = note_repo.get_keydown_combination( adj_event_note, event_vel );
+  auto key_region = k.future.mutable_storage( frames_processed % FUTURE_LENGTH );
+  auto total_region = total_future.mutable_storage( frames_processed % FUTURE_LENGTH );
 
-#if 0
   const size_t max_size = combo.a->size();
-  if ( max_size > k.future.size() ) {
+  if ( max_size > total_region.size() or max_size > key_region.size() or max_size > FUTURE_LENGTH ) {
     throw runtime_error( "note too big!" );
   }
 
   size_t offset = 0;
+  const vector<wav_frame_t>& a = *combo.a;
+  const vector<wav_frame_t>& b = *combo.b;
 
   // Update key future and total future
-  for ( int i = 0 ; i < max_size; i++ ) {
-    float amplitude_multiplier = 0.2; /* to avoid clipping */
-
-    const std::pair<float, float> curr_sample
-      = { combo.a->at( i ).first * combo.a_weight + combo.b->at( i ).first * combo.b_weight,
-          combo.a->at( i ).second * combo.a_weight + combo.b->at( i ).second * combo.b_weight };
+  for ( int i = 0; i < max_size; i++ ) {
+    const std::pair<float, float> curr_sample = { a[i].first * combo.a_weight + b[i].first * combo.b_weight,
+                                                  a[i].second * combo.a_weight + b[i].second * combo.b_weight };
 
     // Update key future
-    k.future.at( get_buff_idx( i ) ).first += curr_sample.first;
-    k.future.at( get_buff_idx( i ) ).second += curr_sample.second;
+    key_region[i].first += curr_sample.first;
+    key_region[i].second += curr_sample.second;
 
     // Update total future
-    total_future.at( get_buff_idx( i ) ).first += curr_sample.first;
-    total_future.at( get_buff_idx( i ) ).second += curr_sample.second;
+    total_region[i].first += curr_sample.first;
+    total_region[i].second += curr_sample.second;
   }
-#endif
 }
 
 void Synthesizer::add_key_release( uint8_t adj_event_note, uint8_t event_vel )
@@ -109,18 +108,17 @@ void Synthesizer::add_key_release( uint8_t adj_event_note, uint8_t event_vel )
   const std::vector<std::pair<float, float>> rel_samples = note_repo.get_wav( true, adj_event_note, event_vel );
   auto key_region = k.future.mutable_storage( frames_processed % FUTURE_LENGTH );
   auto total_region = total_future.mutable_storage( frames_processed % FUTURE_LENGTH );
-  
+
   // Update key future and total future
   for ( size_t offset = 0; offset < 48000 * 30; offset++ ) {
-    auto& key_sample = key_region[ offset ];
-    auto& total_sample = total_region[ offset ];
+    auto& key_sample = key_region[offset];
+    auto& total_sample = total_region[offset];
 
     if ( vol_ratio > 0 )
       vol_ratio -= 0.0001;
 
     if ( !sustain_down ) {
-      const std::pair<float, float> new_sample = { key_sample.first * vol_ratio,
-                                                   key_sample.second * vol_ratio };
+      const std::pair<float, float> new_sample = { key_sample.first * vol_ratio, key_sample.second * vol_ratio };
 
       // Update total future by getting delta of new key future and old key future
       total_sample.first += new_sample.first - key_sample.first;
@@ -149,7 +147,7 @@ void Synthesizer::add_key_release( uint8_t adj_event_note, uint8_t event_vel )
 
 wav_frame_t Synthesizer::get_curr_sample() const
 {
-  return total_future.storage(frames_processed)[0];
+  return total_future.storage( frames_processed )[0];
 }
 
 void Synthesizer::advance_sample()
